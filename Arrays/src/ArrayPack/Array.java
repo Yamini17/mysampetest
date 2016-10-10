11fa3a8f6e61795adb9386589a75afe0db4419c5
Yamini17 <adusu.yamini17@gmail.com>
2016-10-09 19:23:25 -0500

    Add files via upload

---
+++ Arrays/src/ArrayPack/Array.java
@@ -0,0 +1,14 @@
+package ArrayPack;
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a[]={1,5,6,7}; //int[3] is the size of the array
+         
+         System.out.println(a[2]);
+         System.out.println(a[1]);
+         System.out.println(a[0]);
+        
+	}
+}
---
+++ Arrays/src/ArrayPack/ArrayList.java
@@ -0,0 +1,33 @@
+package ArrayPack;
+
+class ArrayList {
+
+	public int Method(){
+			int a=10;
+			return (a);
+			}
+	public int Method2(){
+		int b=15;
+		return (b);
+		}
+	public int Method3(){
+		int c=30;
+		return (c);
+		}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+        ArrayList List= new ArrayList();
+        
+        int d=List.Method();
+        int e=List.Method2();
+        int f=List.Method3();
+        System.out.println("Print the List " +d);
+        System.out.println("Print the List " +e); 
+        System.out.println("Print the List " +f);
+       
+         
+        	}
+
+}
